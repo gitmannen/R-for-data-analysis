b010bc8cdcf1147d0a5c3c0e3d7802571f9eb7af
gitmannen <miscellaneous9900@gmail.com>
2021-07-16 17:31:37 +0800

    updated code

--- RST robustness cell number/Robustness/robustness anova.R
+++ RST robustness cell number/Robustness/robustness anova.R
@@ -1,20 +1,10 @@
 library(ggplot2)
 
 getwd()
-# setwd("..\\Users\\Lion\\Documents\\Lion Local\\RST and RMT qualification\\rst")
 a<-read.csv("RST Robustness cell number.csv")
 a
 str(a)
-#a$Operator == 'O1'
-# a[a$Operator == 'O1',2] <- "Operator 1"
-# a[a$Operator == 'O2',2] <- "Operator 2"
-# 
-# summary(a[a$Operator == "Operator 1",][a$Sample == "PC",])
-# OperatorPC <- a[a$Operator == "Operator 1",][a$Sample == "PC",]
-# mean(OperatorPC)
-# OperatorPC[,4]
-# apply(OperatorPC,2,mean)
-#Box Plot of Intermediate Precision (Operator to Operator) in the ROX channel
+
 summary(a)
 z<-ggplot(data=a, aes(x=Group, y=Ct, label = Ct))
 z+
@@ -62,60 +52,7 @@ Summarize(Ct~Group,
           data=a,
           digits = 3)
 
-# dayToDayVar <- ggplot(data = a, aes(x= Operator, y = ROX))
-# dayToDayVar+
-#   geom_boxplot()+
-#   geom_jitter(aes(colour=Day))
 
-
-#Additional ANOVA test using the R handbook/ Mix of Two-way and One-way ANOVA.
-# if(!require(psych)){install.packages("psych")}
-# if(!require(FSA)){install.packages("FSA")}
-# if(!require(Rmisc)){install.packages("Rmisc")}
-# if(!require(ggplot2)){install.packages("ggplot2")}
-# if(!require(car)){install.packages("car")}
-# if(!require(multcompView)){install.packages("multcompView")}
-# if(!require(lsmeans)){install.packages("lsmeans")}
-# if(!require(rcompanion)){install.packages("rcompanion")}
-# 
-# typeof(a)
-# 
-# library(psych)
-# headTail(a)
-# a
-# 
-# library(FSA)
-# Summarize(ROX~Operator + Sample,
-#           data=a,
-#           digits = 3)        
-# 
-# boxplot(ROX~Operator + Sample,
-#         data = a)
-# 
-# library(rcompanion)
-# Sum = groupwiseMean(data = a,
-#                     var = "ROX",
-#                     group = c("Operator","Sample"),
-#                               conf = 0.95,
-#                               digits = 3,
-#                               tradition = F,
-#                               percentile = T)
-# Sum
-# 
-# library(ggplot2)
-# ggplot(Sum,aes(x = Sample, y = Mean))+
-#   geom_errorbar(aes(ymin = Percentile.lower,
-#                     ymax = Percentile.upper),
-#                 width = 0.05,
-#                 size = 0.5)+
-#   geom_point(shape = 15,
-#               size = 4,
-#              aes(colour = Operator))+
-#   theme_bw()+
-#   theme(axis.title = element_text(face = "bold"))+
-#   ylab("Mean Rox, ct")
-# 
-# 
 model = lm(Ct~Group,
           data = a)
 summary(model)
