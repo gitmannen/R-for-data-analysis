97519cd85d0c0efd57037be65aeeb38b0b5c9ade
gitmannen <miscellaneous9900@gmail.com>
2021-09-24 16:15:28 +0800

    made changes to the plot for linearity and the boxplot
    removed boxplot and used horizontal bars to display the mean for a cleaner look.
    
    added the linear equation and the R2 value on the plot

--- SS Qual/Precision Accuarcy Linearity/Linearity and Box plot.R
+++ SS Qual/Precision Accuarcy Linearity/Linearity and Box plot.R
@@ -5,26 +5,11 @@ getwd()
 a<-read.csv("CD8posVbetapos percent viable after normalization with mock for R.csv")
 a
 str(a)
-#a$Operator == 'O1'
-# a[a$Operator == 'O1',2] <- "Operator 1"
-# a[a$Operator == 'O2',2] <- "Operator 2"
-# 
-# summary(a[a$Operator == "Operator 1",][a$Sample == "PC",])
-# OperatorPC <- a[a$Operator == "Operator 1",][a$Sample == "PC",]
-# mean(OperatorPC)
-# OperatorPC[,4]
-# apply(OperatorPC,2,mean)
-
 
 # setting the factors
-
 summary(a)
 a$Sample
 a$Sample = factor(a$Sample, levels=c('EP0 (Mock)','EP20','EP40','EP60','EP80','EP100'))
-
-
-
-
 
 
 dataset<-a[c(1,6)]
@@ -32,7 +17,9 @@ dataset
 str(dataset)
 dataset$Sample
 avgByEPConc <- aggregate(x=dataset$EP,by=list(dataset$Sample),FUN =mean)
-# aggregate(x=dataset$CD8.Vb3....Viable,by=list(dataset$Sample),FUN =sd)
+daggsd<-aggregate(x=dataset$EP,by=list(dataset$Sample),FUN =sd) # use the aggregate function to pass a function across the data
+daggmean<-aggregate(x=dataset$EP,by=list(dataset$Sample),FUN =mean)
+
 
 avgByEPConc$Group.1 <- c(0,20,40,60,80,100)
 avgByEPConc$Group.1 = factor(avgByEPConc$Group.1, levels=c('0','20','40','60','80','100'))
@@ -43,238 +30,65 @@ colnames(avgByEPConc)=c("EP_Percent","CD8posVb3pos_Percent_Viable","EP")
 
 
 
-# plotting a regression line
+# plotting a regression line, not required as this plots a regression line of the means, not taking the individual values
 linplot <- ggplot(data=avgByEPConc, aes(x=EP, y=CD8posVb3pos_Percent_Viable, label = CD8posVb3pos_Percent_Viable))
 linplot+
   geom_point(aes(x=EP, y=CD8posVb3pos_Percent_Viable))+
   geom_smooth(aes(x=EP, y=CD8posVb3pos_Percent_Viable), method =lm, formula = y~x)
   #stat_summary(aes(x=EP, y=CD8posVb3pos_Percent_Viable))
 
+# getting the regression model. optional but the data can be counter-checked with the result from stat_poly_eq()
+regressor = lm(formula = CD8posVb3pos_Percent_Viable ~ EP,
+                   data = avgByEPConc)
+regressor
 
+# installing ggpubr
+#install.packages("ggpubr")
+library(ggpubr)
 
-
+#install.packages("ggpmisc")
+library(ggpmisc)
 
 dataset2<- a[c(2,6)]
 dataset2$EPConcFactor <- factor(dataset2$EP.Conc, levels=c(0,20,40,60,80,100))
+str(dataset2)
 
-linplot <- ggplot(data=dataset2, aes(x=EP.Conc, y=EP))
-linplot+
-  geom_point(aes(x=EP.Conc, y=EP))+
-  geom_smooth(aes(x=EP.Conc, y=EP), method =lm, formula = y~x)+
+bplot <- ggplot(data=dataset2, aes(x=EP.Conc, y=EP))
+# box plot but using the ggpmisc to give the equation of the regression line
+bplot+
+  geom_smooth(aes(x=EP.Conc,y=EP),method=lm, se = TRUE, formula = y~x,)+
+  #geom_boxplot(aes(colour=EPConcFactor), outlier.alpha = 0)+ # optional, but the graph will be too confusing
+  stat_summary(aes(color=EPConcFactor),fun = mean,geom="crossbar", width = 5)+ # if you don't want to use box plot you can just use this to draw the mean line at each group
+  geom_point(aes(color=EPConcFactor),size=1, alpha=1)+
   scale_x_continuous(name="EP Percent",n.breaks=11,labels=waiver())+
   scale_y_continuous(name="CD8+Vb3+ % Viable",n.breaks=20,labels=waiver())+
-  boxplot(aes(x=dataset2$EPConcFactor, y=EP))
-  
-boxplo
-
-warnings()
-
-
-
-
-
-
-
-
-
-
-#Box Plot of Intermediate Precision (Operator to Operator) for Total (CD8+/-) Vb3+ % Viable
-
-
-z<-ggplot(data=a, aes(x=Day, y=CD8.Vb3....Viable, label = CD8.Vb3....Viable))
-z+
-  facet_wrap(.~Sample,nrow = 1, scales = "free_y", labeller = label_both)+
-  scale_y_continuous(n.breaks = 10)+
-  geom_boxplot(outlier.alpha = 0,
-               alpha = 0.5)+
-  geom_jitter(size = 5,
-              aes(colour = Operator),
-              alpha = 0.3
-              )+
-  ggtitle("Box Plot of Intermediate Precision (Operator to Operator) for Total CD8+ Vb3+ % Viable")+
-  ylab("Total CD8+ Vb3+ % Viable")+
+  stat_poly_eq(aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")),
+               parse = TRUE, coef.digits = 5, f.digits = 5, p.digits = 10, rr.digits = 4)+ # just use this function from ggpmisc to get the equation and r2 value
+  guides(color = guide_legend(title = "EP Percent"))+
+  ggtitle("Linear Regression of CD8+Vb3+ % viable against Percent EP cells in various EP:Mock mixing ratios")+
   theme(axis.title = 
           element_text(size = 18),
         legend.title = 
-          element_text(size = 18),
+          element_text(size = 12),
         legend.text = 
-          element_text(size = 16),
-        strip.text = 
-          element_text(size = 16),
+          element_text(size = 12),
         axis.text = 
-          element_text(size = 16),
-        axis.ticks.x.bottom = 
-          element_blank(),
-        axis.title.x = 
-          element_blank(),
+          element_text(size = 12),
         plot.title =
-          element_text(size = 20,
-                       hjust = 0.5)
-        )+
-  
-  #add in labels to the plot
-  geom_text(check_overlap = T, #avoid overlapping the labels
-            size = 3,
-            show.legend = F, #remove the 'a' in the legend
-            #hjust = 0,
-            #nudge_x = -0.6,
-            #vjust = 0,
-            #nudge_y = -0.01,
-            aes(colour = Operator) #changes the colour to match the Operator colour
-            )
-
+          element_text(size = 16,
+                       hjust = 0.5))#+
+  #coord_cartesian(ylim=c(65,80)) # use this to zoom to the upper limit
   
 
-#Box Plot of Intermediate Precision (Day to Day) for Total (CD8+/-) Vb3+ % Viable
-x<-ggplot(data=a, aes(x=Operator, y=CD8.Vb3....Viable, label = CD8.Vb3....Viable))
-x+
-  facet_wrap(.~Sample,nrow = 1, scales = "free_y", labeller = label_both)+
-  scale_y_continuous(n.breaks = 10)+
-  geom_boxplot(outlier.alpha = 0)+
-  geom_jitter(size = 5,
-              aes(colour = Day),
-              alpha = 0.5
-  )+
-  ggtitle("Box Plot of Intermediate Precision (Day to Day) for Total CD8+ Vb3+ % Viable")+
-  ylab("for Total CD8+ Vb3+ % Viable")+
-  theme(axis.title = 
-          element_text(size = 18),
-        legend.title = 
-          element_text(size = 18),
-        legend.text = 
-          element_text(size = 16),
-        strip.text = 
-          element_text(size = 16),
-        axis.text = 
-          element_text(size = 16),
-        axis.ticks.x.bottom = 
-          element_blank(),
-        axis.title.x = 
-          element_blank(),
-        plot.title =
-          element_text(size = 20,
-                       hjust = 0.5)
-  )+
-  
-  #add in labels to the plot
-  geom_text(check_overlap = T, #avoid overlapping the labels
-            size = 3,
-            show.legend = F, #remove the 'a' in the legend
-            # hjust = 0,
-            # nudge_x = -0.1,
-            #vjust = 0,
-            #nudge_y = -0.01,
-            aes(colour = Day) #changes the colour to match the Operator colour
-  )
-
-
-
-
-#Responsive
-
-#these 2 libraries are needed for the tidyverse evaluation so that you can you can input arguments to the aes()
-#see https://stackoverflow.com/questions/58786357/how-to-pass-aes-parameters-of-ggplot-to-function and https://ggplot2.tidyverse.org/dev/articles/ggplot2-in-packages.html
-install.packages("gapminder")
-library(gapminder)  
-library(rlang)
-
-
-newfunction <- function(xval,grouper){
-  v1<-ggplot(data=a, aes(x=.data[[xval]], y=CD8.Vb3....Viable, label =CD8.Vb3....Viable)) #here you can see how the aes() arguments refer to the arguments in the function
-  
-  #when the channel is ROX, we need to show all three samples, so you need the facet_wrap because there are 3 samples: PC, NTC and Sample
-
-  v1+
-    facet_wrap(.~Sample,nrow = 1, scales = "free_y", labeller = label_both)+
-    scale_y_continuous(n.breaks = 10)+
-    geom_boxplot(outlier.alpha = 0)+
-    geom_jitter(size = 5,
-                aes(colour = .data[[grouper]]),
-                alpha = 0.5
-    )+
-    ggtitle(paste("Box Plot of Intermediate Precision (", grouper, " to ", grouper, ") for Total CD8+ Vb3+ % Viable ",sep =""))+
-    ylab(paste("Total CD8+ Vb3+ % Viable"))+
-    theme(axis.title =
-            element_text(size = 18),
-          legend.title =
-            element_text(size = 18),
-          legend.text =
-            element_text(size = 16),
-          strip.text =
-            element_text(size = 16),
-          axis.text =
-            element_text(size = 16),
-          axis.ticks.x.bottom =
-            element_blank(),
-          axis.title.x =
-            element_blank(),
-          plot.title =
-            element_text(size = 20,
-                         hjust = 0.5)
-    )+
-    
-    #add in labels to the plot
-    geom_text(check_overlap = T, #avoid overlapping the labels
-              size = 3,
-              show.legend = F, #remove the 'a' in the legend
-              # hjust = 0,
-              # nudge_x = -0.1,
-              #vjust = 0,
-              #nudge_y = -0.01,
-              aes(colour = .data[[grouper]]) #changes the colour to match the Operator colour
-    )
-
-  
-
-    
-}
-
-library(FSA)
-aSummary <- Summarize(CD8.Vb3....Viable ~ Sample+Operator,
-                     data=a,
-                     digits = 3)
-
-bSummary <- Summarize(CD8.Vb3....Viable ~ Sample+Day,
-                      data=a,
-                      digits = 3)
-# DAdata<-aSummary[1:6,c(1,2,4)]
-# DAdata$EPPercent <- c("0","100","20","40","60","80")
-# DAdata$EPPercent <- factor(DAdata$EPPercent, levels=c('0','100','20','40','60','80'))
-# 
-# regressor = lm(formula = mean ~ EPPercent,
-#                data = DAdata)
-# 
-# ggplot(data = DAdata, aes(x=EPPercent, y=mean))+
-#   geom_point(aes(x=EPPercent, y=mean))+
-#   geom_line()
-# 
-# 
-# plot(DAdata$EPPercent,DAdata$mean)
-
-aSummary
-bSummary
-
-
-modela = lm(CD8.Vb3....Viable~Operator+Day+Sample,
-            data = a)
-
-summary(modela)
-
-library(car)
-Anova(modela,
-      type = "II")
-
-library(multcompView)
-library(lsmeans)
-leastsquare = lsmeans(modela, pairwise ~ Operator + Day + Sample, adjust = "tukey")
-leastsquare
-
-
-
-
-newfunction("Operator","Day")
-newfunction("Day","Operator") #arguments are as follows (xval, channel, grouper)
-# xval is what you want the size of the box plot to depend on. e.g. if you are looking at day to day variability,
-# the xval should be the operator, so you will see 1 box plot for operator 1 and another boxplot for operator 2 the factor you are comparing between (e.g. day to day or operator to operator)
-# channel is the channel that you are interested in (e.g. ROX or FAM)
-# grouper is what you want to compare. e.g. day to day or operator to operator. if you put day, then you should see that you the points in your box plot is colour cooded according to the day of operation
+# bplot
+# bplot+
+#   #geom_point(aes(x=EP.Conc, y=EP))+
+#   #geom_smooth(aes(x=EP.Conc, y=EP), method =lm, formula = y~x)+
+#   geom_boxplot(data=dataset2, aes(group=EPConcFactor), outlier.alpha = 0)+
+#   geom_jitter(aes(color=EPConcFactor))+
+#   scale_x_continuous(name="EP Percent",n.breaks=11,labels=waiver())+
+#   scale_y_continuous(name="CD8+Vb3+ % Viable",n.breaks=20,labels=waiver())+
+#   geom_smooth(aes(x=EP.Conc,y=EP),method=lm, se = FALSE, formula = y~x)+
+#   stat_cor(label.y = 80,digits = 3) + # stat_cor uses the ggpubr package
+#   stat_regline_equation(label.y = 77) # stat_regline_equation uses the ggpubr package, but the number of sig digits cannot be cahnged
+#   #geom_line(aes(x= avgByEPConc$EP_Percent ,y=predict(regressor)))
